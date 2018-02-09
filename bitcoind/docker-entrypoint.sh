120cdf5a95f60adf03f90bed6f403f6aa652892f
Hasan Basri <hbasria@gmail.com>
2018-02-09 16:57:09 +0300

    bitcoind files added

---
+++ bitcoind/docker-entrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# exit from script if error was raised.
+set -e
+
+BITCOIN_DATA=${BITCOIN_DATA:-/home/bitcoin/.bitcoin}
+
+BITCOIN_RPC_USER=${BITCOIN_RPC_USER:-bitcoin}
+
+BITCOIN_RPC_PASSWORD=${BITCOIN_RPC_PASSWORD:-password}
+
+BITCOIN_RPC_ALLOW_IP=${BITCOIN_RPC_ALLOW_IP:-::/0}
+
+BITCOIN_PRINT_TO_CONSOLE=${BITCOIN_PRINT_TO_CONSOLE:-1}
+
+BITCOIN_DEBUG=${BITCOIN_DEBUG:-info}
+
+PARAMS=$(echo \
+    "-datadir=$BITCOIN_DATA" \
+    "-rpcuser=$BITCOIN_RPC_USER" \
+    "-rpcpassword=$BITCOIN_RPC_PASSWORD" \
+    "-rpcallowip=$BITCOIN_RPC_ALLOW_IP" \
+    "-debug=$BITCOIN_DEBUG"
+)
+
+if [ "$BITCOIN_PRINT_TO_CONSOLE" = "1" ]; then
+    PARAMS="$PARAMS -printtoconsole"
+fi
+
+# Add user parameters to command.
+PARAMS="$PARAMS $@"
+
+# Print command and start bitcoin node.
+echo "Command: $1 $PARAMS"
+
+if [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ] || [ "$1" = "bitcoind" ]; then
+    mkdir -p "$BITCOIN_DATA"
+
+    if [ ! -s "$BITCOIN_DATA/bitcoin.conf" ]; then
+        cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
+datadir=${BITCOIN_DATA:-/home/bitcoin/.bitcoin}
+rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
+printtoconsole=${BITCOIN_PRINT_TO_CONSOLE:-1}
+rpcallowip=${BITCOIN_RPC_ALLOW_IP:-::/0}                
+EOF
+        chown -R bitcoin:bitcoin "$BITCOIN_DATA"
+    fi
+
+    exec gosu bitcoin "$@"
+else
+    exec "$@"    
+fi
+
+
+
+
